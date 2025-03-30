f3c1d98bf5120710e3e8b0aee10d120130c5b49b
Shuudy <ancieux.jerome059@gmail.com>
2025-03-30 13:04:51 +0200

    fix(sql): ajuster les tailles des colonnes description et categorie
    Correction des tailles des colonnes description (100) et categorie (30) pour correspondre au dictionnaire de données.

--- sql/stock.sql
+++ sql/stock.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS stock_total;
 CREATE TABLE produit (
     id_produit INT AUTO_INCREMENT PRIMARY KEY,
     nom_produit VARCHAR(100) NOT NULL,
-    description VARCHAR(255), 
+    description VARCHAR(100), 
     quantite INT,
     prix DECIMAL(10,2) NOT NULL,
-    categorie VARCHAR(50),
+    categorie VARCHAR(30),
 );
 
 CREATE TABLE stock_total (
