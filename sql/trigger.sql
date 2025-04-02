0f67472b9ab9904761871452d742fd4a513dabbc
mnoella <mailholnoella@gmail.com>
2025-04-02 10:50:46 +0200

    correction des trigger apres modification des procédures

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,21 +1,24 @@
 DELIMITER //
-
-CREATE TRIGGER update_product_quantity
-AFTER INSERT ON commande
+CREATE TRIGGER after_produit_insert
+AFTER INSERT ON produit
 FOR EACH ROW
 BEGIN
-    DECLARE @quantite_produit INT;
-
-    SELECT quantite FROM produit WHERE id_produit = NEW.id_produit INTO @quantite_produit;
-
-    SET @quantite_produit = @quantite_produit - -- Quantité de produit acheté dans la commande --;
-
-    IF @quantite_produit < 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Quantité de produit insuffisante dans le stock.';
-    END IF;
-    
-    UPDATE produit SET quantite = @quantite_produit WHERE id_produit = NEW.id_produit;
-    UPDATE stock_total SET total_stock = total_stock - -- Quantité de produit acheté dans la commande -- WHERE id_stock_total = 1;
+    CALL calculerStockTotal();
 END //
 
+
+CREATE TRIGGER after_produit_insert
+AFTER UPDATE ON produit
+FOR EACH ROW
+BEGIN
+    CALL calculerStockTotal();
+END //
+
+
+CREATE TRIGGER after_produit_insert
+AFTER DELETE ON produit
+FOR EACH ROW
+BEGIN
+    CALL calculerStockTotal();
+END //
 DELIMITER ;
