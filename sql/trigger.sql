da68fea00063b0c6a4c54ab1dd67d020266d1b7d
Shuudy <ancieux.jerome059@gmail.com>
2025-04-02 09:20:40 +0200

    fix(sql): ajouter une vérification pour la quantité de produit insuffisante dans le stock

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -10,6 +10,10 @@ BEGIN
 
     SET @quantite_produit = @quantite_produit - -- Quantité de produit acheté dans la commande --;
 
+    IF @quantite_produit < 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Quantité de produit insuffisante dans le stock.';
+    END IF;
+    
     UPDATE produit SET quantite = @quantite_produit WHERE id_produit = NEW.id_produit;
     UPDATE stock_total SET total_stock = total_stock - -- Quantité de produit acheté dans la commande -- WHERE id_stock_total = 1;
 END //
